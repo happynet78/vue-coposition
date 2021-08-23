07f2757da135c522adf120138872e88849fbc567
happynet78 <49040138+happynet78@users.noreply.github.com>
2021-08-23 23:41:38 +0900

    VUE-Composition
    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<!-- <Data /> -->
+	<!-- <DemoOne /> -->
+	<!-- <DemoTwo /> -->
+	<!-- <Methods /> -->
+	<VModel />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Data from './components/Data.vue'
+// import DemoOne from './components/DemoOne.vue'
+// import DemoTwo from './components/DemoTwo.vue'
+// import Methods from './components/Methods.vue'
+import VModel from './components/VModel.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		// Data,
+		// DemoOne,
+		// DemoTwo,
+		// Methods,
+		VModel,
+	}
 }
 </script>
 
