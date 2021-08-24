fe33eae70893cc8e06bc1669651035e3ad8156a2
happynet78 <49040138+happynet78@users.noreply.github.com>
2021-08-24 20:11:59 +0900

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-	<!-- <Data /> -->
-	<!-- <DemoOne /> -->
-	<!-- <DemoTwo /> -->
-	<!-- <Methods /> -->
-	<!-- <VModel /> -->
-	<!-- <Computed /> -->
-	<!-- <Watch /> -->
+	<Data />
+	<DemoOne />
+	<DemoTwo />
+	<Methods />
+	<VModel />
+	<Computed />
+	<Watch />
 	<ProvideInject />
 </template>
 
 <script>
-// import Data from './components/Data.vue'
-// import DemoOne from './components/DemoOne.vue'
-// import DemoTwo from './components/DemoTwo.vue'
-// import Methods from './components/Methods.vue'
-// import VModel from './components/VModel.vue'
-// import Computed from './components/Computed.vue'
-// import Watch from './components/Watch.vue'
+import Data from './components/Data.vue'
+import DemoOne from './components/DemoOne.vue'
+import DemoTwo from './components/DemoTwo.vue'
+import Methods from './components/Methods.vue'
+import VModel from './components/VModel.vue'
+import Computed from './components/Computed.vue'
+import Watch from './components/Watch.vue'
 import ProvideInject from './components/ProvideInject.vue'
 
 export default {
 	name: 'App',
 	components: {
-		// Data,
-		// DemoOne,
-		// DemoTwo,
-		// Methods,
-		// VModel,
-		// Computed,
-		// Watch,
+		Data,
+		DemoOne,
+		DemoTwo,
+		Methods,
+		VModel,
+		Computed,
+		Watch,
 		ProvideInject,
 	}
 }
